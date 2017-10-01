4d211a5ecd42cc1115f960aed22a63c2b0f6ed84
tom-code <petrilak.tomas@gmail.com>
2017-10-01 15:08:48 +0200

    another travis test

--- run_ir/compile.sh
+++ run_ir/compile.sh
@@ -1,5 +1,11 @@
-export CXX=clang++
-$CXX run_ir.cc -std=c++11 -lLLVM-5.0 -g
+#export CXX=clang++
+
+set -x
+CXX=${CXX:=clang++}
+LCFG=${LCFG:=llvm-config}
+
+$LCFG --cxxflags
+$CXX run_ir.cc `$LCFG --cxxflags` -std=c++11  -g `$LCFG --libs`
 
 #$CXX run_ir.cc -std=c++14 `llvm-config-5.0 --cxxflags --ldflags --system-libs --libs all` -g
 
